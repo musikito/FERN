9a9a1784fb080caa98939dada86ee1fba8e181fa
jose marte <tumusicalatina@gmail.com>
2019-06-21 13:53:01 -0400

    Friday

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PasswordForgetPage = () => (
+  <div>
+    <h1>Forgot Password?</h1>
+  </div>
+);
+
+export default PasswordForgetPage;
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUpPage = () => (
+  <div>
+    <h1>Sign Up</h1>
+  </div>
+);
+
+export default SignUpPage;
